61e5bd1b9a90d43f2584d8728481e2e633fb0eba
Zhongheng-Cheng <zc2737@columbia.edu>
2024-08-03 15:41:21 -0400

    added external ip access

--- app.py
+++ app.py
@@ -55,4 +55,4 @@ def get_recommand_question():
     return jsonify({'recommand_question': response})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
