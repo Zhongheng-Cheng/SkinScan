b4159e81c88058ba7dfc43c96c22d12988b1ba5c
Zhongheng-Cheng <zc2737@columbia.edu>
2024-09-03 19:14:55 -0400

    added endless round chatting and formatted terminal output

--- test_doctor_search.py
+++ test_doctor_search.py
@@ -28,37 +28,43 @@ chat = model.start_chat()
 # prompt = "Hi, how are you doing?"
 # prompt = "I have some problems with my face"
 # prompt = "I am in New York, which doctor can I turn to right now?"
-prompt = "How can I treat this problem on my face?"
+# prompt = "How can I treat this problem on my face?"
 # prompt = "Can you recommend some product for me to use?"
 
-response = chat.send_message(prompt)
-print(response.parts)
+def chat_once(prompt):
+    response = chat.send_message(prompt)
 
-# if Gemini is calling the function
-# it will return the function name and args instead of calling it directly
-func_outputs = {}
-for part in response.parts:
-    if fn := part.function_call:
-        args = ", ".join(f"{key}={val}" if type(val) != str else f"{key}='{val}'" for key, val in fn.args.items())
+    # if Gemini is calling the function
+    # it will return the function name and args instead of calling it directly
+    func_outputs = {}
+    for part in response.parts:
+        if text := part.text:
+            print(text)
+        elif fn := part.function_call:
+            args = ", ".join(f"{key}={val}" if type(val) != str else f"{key}='{val}'" for key, val in fn.args.items())
 
-        # manually call the function
-        expression = f"{fn.name}({args})"
-        print(expression)
-        result = eval(expression)
+            # manually call the function
+            expression = f"{fn.name}({args})"
+            print("Function calling: " + expression)
+            result = eval(expression)
 
-        # storing the function output
-        print(result)
-        func_outputs[fn.name] = result
+            # storing the function output
+            # print(result)
+            func_outputs[fn.name] = result
 
-# feed the functions and outputs back into Gemini to generate text response
-if func_outputs:
-    response_parts = [
-        genai.protos.Part(function_response=genai.protos.FunctionResponse(name=fn, response={"result": val}))
-        for fn, val in func_outputs.items()
-    ]
+    # feed the functions and outputs back into Gemini to generate text response
+    if func_outputs:
+        response_parts = [
+            genai.protos.Part(function_response=genai.protos.FunctionResponse(name=fn, response={"result": val}))
+            for fn, val in func_outputs.items()
+        ]
 
-    response = chat.send_message(response_parts)
-    print(response.text)
+        response = chat.send_message(response_parts)
+        print(response.text)
+
+while True:
+    prompt = input(">>> ")
+    chat_once(prompt)
 
 
 # Reference: https://ai.google.dev/gemini-api/docs/function-calling/tutorial?lang=python
